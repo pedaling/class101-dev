2f6b95e5bbd213aa1459031588c7d601f6cb3581
yozzing <ddalpange@gmail.com>
2019-05-10 18:02:11 +0900

    now 배포 추가

--- src/utils/users.js
+++ src/utils/users.js
@@ -20,7 +20,7 @@ const users = [
   {
     profileImage: '/images/profiles/yozzing.jpeg',
     name: '김요한',
-    description: '개발떌감입니다',
+    description: '김요찡찡요찡찡',
     github: 'https://github.com/ddalpange',
   },
 ];
