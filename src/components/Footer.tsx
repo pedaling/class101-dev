baed7126471d711bbd92170ed239cb468f007cfe
yohan <ddalpange@gmail.com>
2020-02-26 10:39:52 +0900

    update design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,28 +11,33 @@ const Footer: React.FC = () => {
       <Grid>
         <Row>
           <Col>
-            <LogoIcon src="/images/logotype-black.png" alt="class101" />
-            <ContactSection>
-              <ContactArticle>
-                <b>{t('title')}</b>
-                <p>{t('address')}</p>
-                <p>457-81-00277</p>
-                <p>
-                  <a href="mailto:helloworld@class101.net">helloworld@class101.net</a>
-                </p>
-              </ContactArticle>
-              <ContactArticle>
-                <a href="https://play.google.com/store/apps/details?id=net.pedaling.class101" target="_blank">
-                  <DownloadIcon src="/images/download-android.png" alt="android" />
+            <ContactArticle>
+              <b>{t('title')}</b>
+              <p>{t('address')}</p>
+              <p>457-81-00277</p>
+              <p>
+                <a href="mailto:helloworld@class101.net">
+                  helloworld@class101.net
                 </a>
-                <a
-                  href="https://apps.apple.com/kr/app/class101-%ED%81%B4%EB%9E%98%EC%8A%A4101/id1320607634"
-                  target="_blank"
-                >
-                  <DownloadIcon src="/images/download-ios.png" alt="ios" />
-                </a>
-              </ContactArticle>
-            </ContactSection>
+              </p>
+            </ContactArticle>
+            <ContactArticle>
+              <a
+                href="https://play.google.com/store/apps/details?id=net.pedaling.class101"
+                target="_blank"
+              >
+                <DownloadIcon
+                  src="/images/download-android.png"
+                  alt="android"
+                />
+              </a>
+              <a
+                href="https://apps.apple.com/kr/app/class101-%ED%81%B4%EB%9E%98%EC%8A%A4101/id1320607634"
+                target="_blank"
+              >
+                <DownloadIcon src="/images/download-ios.png" alt="ios" />
+              </a>
+            </ContactArticle>
           </Col>
         </Row>
       </Grid>
@@ -43,12 +48,14 @@ const Footer: React.FC = () => {
 export default Footer;
 
 const Container = styled.footer`
-  background-color: ${Colors.gray100};
-  padding-top: 32px;
+  ${TextStyles.body2};
+  background-color: ${Colors.gray900};
+  color: ${Colors.gray100};
+  padding-top: 16px;
   a {
     color: inherit;
     font-weight: 600;
-    color: ${Colors.gray900};
+    color: ${Colors.gray200};
     text-decoration: none;
     &:hover {
       color: inherit;
@@ -61,20 +68,11 @@ const Container = styled.footer`
   }
 `;
 
-const LogoIcon = styled.img`
-  width: auto;
-  height: 40px;
-  margin: 0;
-`;
-
 const DownloadIcon = styled.img`
   width: 120px;
   margin-right: 16px;
 `;
 
-const ContactSection = styled.div`
-  ${TextStyles.body1};
-`;
 const ContactArticle = styled.div`
-  padding: 16px 0;
+  padding: 8px 0;
 `;
