d5623a7214fcdcac9782ab9a8d823fe58553a311
Dao <me@kdw.kr>
2020-10-07 14:46:47 +0900

    fix: i18n json파일을 불러오도록 수정한다

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,9 @@ import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import translationKO from './ko.json';
+import translationEN from './en.json';
+
 export const initI18n = () => {
   i18n
     .use(detector)
@@ -13,69 +16,10 @@ export const initI18n = () => {
       },
       resources: {
         ko: {
-          translation: {
-            profile: {
-              name: {
-                yozzing: '요찡',
-                john: '존',
-                joy: '조이',
-                chichi: '치치',
-                lama: '라마',
-                esmond: '에즈먼드',
-                donut: '도넛',
-                hoi: '호이',
-                grep: '그랩',
-                cheolee: '처리',
-                zzung: '쩡',
-                tony: '토니',
-                lion: '라이언',
-                hun: '훈',
-                han: '한',
-                rogiry: '로기리'
-              }
-            },
-            title: '클래스101 기술 블로그',
-            description: '신나는 코딩세상',
-            company: '(주)클래스101',
-            address: '서울특별시 중구 통일로 10, 연세재단세브란스빌딩 18층',
-            tel: '457-81-00277',
-            searchPlaceholder: '제목 및 내용을 입력하세요',
-            recruiting: '채용',
-            members: '구성원'
-          }
+          translation: translationKO
         },
         en: {
-          translation: {
-            profile: {
-              name: {
-                yozzing: 'Yozzing',
-                john: 'John',
-                joy: 'Joy',
-                chichi: 'Chichi',
-                lama: 'Lama',
-                esmond: 'Esmond',
-                donut: 'Donut',
-                hoi: 'Hoi',
-                grep: 'Grep',
-                cheolee: 'Cheolee',
-                zzung: 'Zzung',
-                tony: 'Tony',
-                lion: 'Lion',
-                hun: 'Hun',
-                han: 'Han',
-                rogiry: 'Rogiry'
-              }
-            },
-            title: 'Class101 Tech Blog',
-            description: 'Funny Coding World',
-            company: 'Class101',
-            address:
-              '13 floor, seoul-square, (416), han-gang-dae-ro, jun-gu, seoul',
-            tel: '457-81-00277',
-            searchPlaceholder: 'Input title or contents',
-            recruiting: 'Recruiting',
-            members: 'Members'
-          }
+          translation: translationEN
         }
       },
       fallbackLng: 'ko',
